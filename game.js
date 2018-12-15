88b083b6bc23e5c690a6047dedfa0ba48a8f0c44
Github12121 <lukecmcaree@gmail.com>
2018-12-15 09:57:49 -0500

    Added movement

--- game.js
+++ game.js
@@ -10,10 +10,10 @@ var BootScene = new Phaser.Class({
     preload: function ()
     {
         // map tiles
-        this.load.image('tiles', 'assets/map/spritesheet.png');
+        this.load.image('tiles', 'assets/spritesheet.png');
 
         // map in json format
-        this.load.tilemapTiledJSON('map', 'assets/map/map.json');
+        this.load.tilemapTiledJSON('map', 'assets/map.json');
 
         // our two characters
         this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
@@ -25,7 +25,101 @@ var BootScene = new Phaser.Class({
     }
 });
 
-var WorldScene = new Phaser.Class({});
+var WorldScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize: function BootScene () {
+        Phaser.Scene.call(this, { key: 'WorldScene' });
+    },
+
+    preload: function () {
+
+    },
+
+    create: function () {
+        var map = this.make.tilemap({key: 'map'});
+        var tiles = map.addTilesetImage('spritesheet', 'tiles');
+
+        var grass = map.createStaticLayer('Grass', tiles, 0, 0);
+        var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
+        obstacles.setCollisionByExclusion([-1]);
+
+        this.player = this.physics.add.sprite(50, 100, 'player', 6);
+
+        this.physics.world.bounds.width = map.widthInPixels;
+        this.physics.world.bounds.height = map.heightInPixels;
+        this.player.setCollideWorldBounds(true);
+
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.roundPixels = true;
+
+        //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+    },
+    update: function (time, delta) {
+        this.player.body.setVelocity(0);
+
+        // Horizontal movement
+        if (this.cursors.left.isDown)
+        {
+            this.player.body.setVelocityX(-80);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.body.setVelocityX(80);
+        }
+        // Vertical movement
+        if (this.cursors.up.isDown)
+        {
+            this.player.body.setVelocityY(-80);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.body.setVelocityY(80);
+        }
+
+        if (this.cursors.left.isDown)
+        {
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.anims.play('right', true);
+        }
+        else if (this.cursors.up.isDown)
+        {
+            this.player.anims.play('up', true);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.anims.play('down', true);
+        }
+        else
+        {
+            this.player.anims.stop();
+        }
+
+    }
+
+});
 
 console.log(BootScene);
 console.log(WorldScene);
